1ecd54f0451b476c519e467c59590ad5a5c5c379
Eman Atia <emanatiya87@gmail.com>
2025-12-01 04:13:00 +0200

    header margin

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import { Carousel } from "flowbite-react";
 export default function Header() {
   return (
     <>
-      <header className="w-[80%] overflow-hidden h-42 mx-auto rounded-2xl flex items-center justify-center">
+      <header className="w-[80%] overflow-hidden h-42 mx-auto rounded-2xl flex items-center justify-center mt-2">
         <Carousel slide={true} className="w-full h-full">
           <img src="/images/images.png" alt="download1" />
         </Carousel>
